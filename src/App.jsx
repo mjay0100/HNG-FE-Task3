d3bf2f5e8b0991f4958d889196e4d0c58f1abe90
mustapha jamil <mustaphajay2@gmail.com>
2023-09-18 23:40:05 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Home from "./pages/home";
+import Login from "./pages/login";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
